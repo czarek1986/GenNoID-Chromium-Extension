65e6eb8a78db35b1fa3a7e3a6e9f269d4fc0276c
Cezary Kąpicki <cezary.kapicki@gmail.com>
2015-03-09 22:12:15 +0100

    - poprawione rozpoznawanie klikniętego elementu - filtrowanie elementów input tylko do pól w których można wprowadzać tekst

--- content.js
+++ content.js
@@ -3,14 +3,15 @@ var clickedEl = null;
 document.addEventListener("mousedown", function(event){
 	clickedEl = null;
     //right click
-    if(event.button == 2) { 
-		if(event.target.tagName.toLowerCase() == "input" || event.target.tagName.toLowerCase() == "textarea") {
-			clickedEl = event.target;
+    if(event.button == 2) {
+		var tempEl = $(event.target);
+		if(tempEl.is("input:text, input[type=number], input[type=search], input:not([type]), textarea")) {
+			clickedEl = $(event.target);
 		}
     }	
 }, true);
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-		if(clickedEl != null && request != null) 
-			$(clickedEl.tagName + "[name=\"" + clickedEl.name + "\"]").val(request.generate);
+		if(clickedEl != null && clickedEl.length != 0 && request != null) 
+			clickedEl.val(request.generate);
 });
